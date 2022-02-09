c9e3e8c489f4a9b41247db62c01e9c817ccf6783
Viney <vineykhaneja999@gmail.com>
2022-02-09 14:00:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoinSimulation
+{
+    internal class HEAD_TAIL_GAME
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to flip Coin Simulation Program");
+            Console.ReadLine();
+        }
+    }
+}
